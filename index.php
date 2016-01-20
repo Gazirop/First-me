4ad7f223ed975b6d41e64ad938cd108be4936aa5
Victor A <sliv2001@ukr.net>
2016-01-20 19:42:06 +0200

    My first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+</head>
+<body>
+<form method="POST" enctype="multipart/form-data"/>
+<input type="file" name="img[]" multiple/>
+<br>
+    <input type="submit"/>
+</br>
+</form>
+<?php var_dump ($_FILES['img']); ?>
+</body>
+</html>
+
